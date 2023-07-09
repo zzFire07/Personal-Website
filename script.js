1b7e73dd1548381f11a9ca989333fd32a48d0d4a
giorgio.san007 <giorgio.san007@gmail.com>
2023-07-09 20:14:31 -0300

    feature\personalBox: Se personaliza PersonalBox para que solo baje cierta cantidad de pixelescuando se scrollea y luego se quede ahi, y suba cuando vuelva a subir con el scroll. feature\personalPage: Se agrega efecto visual para que quede mas cool

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     colorSwitch = document.querySelector('.set-dark-mode');
     colorSwitch.addEventListener('change', cambiaTema);
+    personalBox = document.querySelector('.personal-info-box');
+    window.addEventListener('scroll', movimientoPersonalBox);
 });
     
 var colorSwitch; // Variable para el interruptor DARK MODE
@@ -17,6 +19,25 @@ var colorSwitch; // Variable para el interruptor DARK MODE
         document.documentElement.setAttribute('tema', 'light');
         console.log('light')
       }
+      console.log(personalBox);
+      console.log(window.scrollY)
     }
+
+    function movimientoPersonalBox() {
+
+    var initialOffset = personalBox.getBoundingClientRect().offsetTop;
+    var maxOffset = 300;  
+
+    var scrollPosition = window.scrollY;
+
+  if (scrollPosition <= maxOffset) {
+    personalBox.style.position = 'fixed';
+    personalBox.style.padding =  '0px';
+    personalBox.style.top = initialOffset + scrollPosition + 'px';
+  } else {
+    personalBox.style.position = 'absolute';
+    personalBox.style.paddingTop =  maxOffset + 'px';
+  }
+}
   
     
