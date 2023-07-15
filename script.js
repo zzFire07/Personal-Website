1675344500080fcab900f152b8c7fe5f3b743844
giorgio.san007 <giorgio.san007@gmail.com>
2023-07-15 20:30:51 -0300

    feature/separator: Se agrega un separador entre cada seccion, el cual progresa hasta llegar al maximo del ancho de la pagina segun el porcentaje que ha scrolleado el usuario la pagina. fix/responsive design: Se ajusta algunos elementos los cuales se comportaban de mala manera en dispositivos con poca anchura. fix/organizacion y comentarios en codigo: Se organiza mejor el codigo y se utiliza mas comentarios en el archivo de JS.

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-    colorSwitch = document.querySelector('.set-dark-mode');
-    colorSwitch.addEventListener('change', cambiaTema);
-    personalBox = document.querySelector('.personal-info-box');
-    window.addEventListener('scroll', movimientoPersonalBox);
+    colorSwitch = document.querySelector('.set-dark-mode'); // Selecciono el interruptor de dark mode
+    colorSwitch.addEventListener('change', cambiaTema); // Utilizo la funcion cambiaTema cada vez que el boton es presionado
+
+
+    personalBox = document.querySelector('.personal-info-box'); // Selecciono la caja aside con mis datos personales.
+    window.addEventListener('scroll', movimientoPersonalBox); // utilizo la funcion de movimiento junto con el scroll de la pagina
+
+
+    bigDiv = document.querySelector('.bg-div'); // Selecciono el div que contiene todo el contenido de la pagina
+
+
+    sectionSeparator = document.querySelectorAll('.section-scroll-progress'); // Selecciono todos los separadores de secciones
+    window.addEventListener('scroll', movimientoSectionSeparator) // utilizo la funcion de movimiento junto con el scroll de la pagina
 });
     
 var colorSwitch; // Variable para el interruptor DARK MODE
     
     function imprimir() { // Para uso de debug
-        console.log(colorSwitch);
+        console.log("");
     }
 
-    function cambiaTema(ev) {
-      if (ev.target.checked) {
+    // Esta funcion cambia el tema del documento, entre un tema claro y uno obscuro.
+    function cambiaTema(ev) { // Se utiliza el evento de "change" del boton como parametro
+      if (ev.target.checked) { // Si el boton se presiona, se cambia el atributo "tema" del html a "dark"
         document.documentElement.setAttribute('tema', 'dark');
         console.log('dark')
       } else {
         document.documentElement.setAttribute('tema', 'light');
         console.log('light')
       }
-      console.log(personalBox);
-      console.log(window.scrollY)
     }
 
+    // Esta funcion mueve la caja con mis datos personales, para que este visible solo un tramo.
     function movimientoPersonalBox() {
 
-    var initialOffset = personalBox.getBoundingClientRect().offsetTop;
-    var maxOffset = 300;  
+    var initialOffset = personalBox.getBoundingClientRect().offsetTop; // Obtiene la distancia entre la caja y el borde superior de la pagina
+    var maxOffset = 300;  // Establece como 300px la distancia maxima que se puede mover la caja
 
-    var scrollPosition = window.scrollY;
+    var scrollPosition = window.scrollY; // Obtiene la posicion del scroll
 
-  if (scrollPosition <= maxOffset) {
+  if (scrollPosition <= maxOffset) { // Si la posicion del scroll es menor a la distancia maxima, se mueve la caja
     personalBox.style.position = 'fixed';
     personalBox.style.padding =  '0px';
     personalBox.style.top = initialOffset + scrollPosition + 'px';
-  } else {
+  } else {  // Si la posicion del scroll es mayor a la distancia maxima, se fija la caja en la distancia maxima
     personalBox.style.position = 'absolute';
     personalBox.style.paddingTop =  maxOffset + 'px';
   }
 }
+
+    // Esta funcion mueve los separadores de seccion, para que se vea el progreso de la pagina.
+    function movimientoSectionSeparator() {
+
+        var scrollPosition = window.scrollY; // Obtiene la posicion del scroll
+
+        var bgDivHeight = bigDiv.getBoundingClientRect().height; // Obtiene la altura del div que contiene todo el contenido de la pagina
+
+        sectionSeparator.forEach(function(element) { // Para cada separador de seccion, se cambia el ancho del elemento
+          element.style.width = (scrollPosition / bgDivHeight * 170) + '%';
+        });
+    }
   
     
